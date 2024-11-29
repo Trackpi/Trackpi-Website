117b80674d1311b8ea51273a9a9699452bc0d832
Yadu Krishnan <yadukrishnan247@gmail.com>
2024-11-29 17:34:32 +0530

    About section alignment and spacing changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import OurTeam from './OurTeam'
-
+import React from 'react';
+import OurTeam from './OurTeam';
 
 function About() {
   return (
     <>
-      <section className='flex justify-center items-center p-3 w-full h-screen bg1'>
-        <div className='flex flex-col items-center'>
-          <h1 className='text-5xl md:text-7xl hover:text-sky-500 home-text'>
+      <section className="flex justify-center items-center p-3 w-full h-screen bg1">
+        <div className="flex flex-col items-center">
+          <h1 className="text-5xl md:text-7xl hover:text-sky-500 home-text">
             About Us
           </h1>
         </div>
       </section>
 
-      <section className='mt-36'>
+      <section className="mt-5">
         <OurTeam />
       </section>
-
     </>
-  )
+  );
 }
 
-export default About
+export default About;
