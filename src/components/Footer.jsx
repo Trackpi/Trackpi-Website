a3a891fe9f26e21493b14d289f7c6b9d1b99f1d5
Najil Rahman PM <sunaidnk1.gmail.com>
2024-12-13 19:30:40 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './footer.css'
-import { linearGradient } from 'framer-motion/client';
-import { color } from 'framer-motion';
+
 function Footer() {
   const videoRefs = useRef([]);
 
