6dddc806b719ab0b3a177ef4df056fa5a0bd3cab
abhishek-475 <61498092+abhishek-475@users.noreply.github.com>
2024-12-13 21:54:47 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { SiMedium } from "react-icons/si";
