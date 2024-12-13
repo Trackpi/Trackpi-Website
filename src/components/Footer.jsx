9ea296c1a6a81f092b3bb038f191b9c1148d6b28
abhishek-475 <61498092+abhishek-475@users.noreply.github.com>
2024-12-13 21:06:31 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React, {  useRef } from "react";
+import React, { useRef } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { SiMedium } from "react-icons/si";
@@ -10,9 +10,6 @@ import { RiFacebookCircleLine } from "react-icons/ri";
 import { TbBrandLinkedin } from "react-icons/tb";
 import { IoMail } from "react-icons/io5";
 import { FaSquarePhone } from "react-icons/fa6";
-
-
-
 
 
 
