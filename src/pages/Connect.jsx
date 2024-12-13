8a92eaaa7f425bab7f5f85d4f8e04340dc16cb2b
keerthanacm <cmkeerthana2001@gmail.com>
2024-12-14 01:39:39 +0530

    connect us page changes

--- src/pages/Connect.jsx
+++ src/pages/Connect.jsx
@@ -6,7 +6,7 @@ import ConnectButtons from '../components/ConnectButtons';
 
 function Connect() {
   return (
-    <div class="first">
+    <div className="first">
       <section className="flex justify-center items-center p-3 w-full h-screen connect_section bg1 relative">
         <div className="flex flex-col items-center z-10">
           <h1 className="text-5xl md:text-7xl working_heading mb-5">
@@ -21,57 +21,61 @@ function Connect() {
         <h1>Contact Us</h1>
             
         <div className='aa'>    
-          <div class="card-group cards">
-              <div class="card card1">
-                <div class="icon">
-                   <i className="fa-solid fa-location-dot me-2"></i>
+          <div className="card-group cards rounded">
+              <div className="card card1 pt-4 rounded-2xl">
+                <div className="icon">
+                   <i className="fa-solid fa-location-dot icon-color"></i>
                 </div>
-                {/* <img src="..." class="card-img-top" alt="..."> */}
-                <div class="card-body">
-                  <h5 class="card-title">Our Office Location</h5>
-                  <p class="card-text">Kakkanad, Kochi, India</p>
+               
+                <div className="card-body">
+                  <h6 className="card-title">Our Office Location</h6>
+                  <p className="card-text fw-bold">Kakkanad, Kochi, India</p>
                 </div>
                 
               </div>
-              <div class="card card1">
-                 <div class="icon">
-                   <i className="fa-solid fa-phone me-2"></i> 
+              <div className="card card1 pt-4 ">
+                 <div className="icon">
+                   <i className="fa-solid fa-phone icon-color"></i> 
                  </div>
-                {/* <img src="..." class="card-img-top" alt="..."> */}
-                <div class="card-body">
-                  <h5 class="card-title">Phone</h5>
-                  <p class="card-text">+91 8078179646</p>
+               
+                <div className="card-body">
+                  <h6 className="card-title">Phone</h6>
+                  <p className="card-text fw-bold">+91 8078179646</p>
                 </div>
               
               </div>
-              <div class="card card1">
-                  <div class="icon">
-                     <i className="fa-solid fa-envelope me-2"></i> 
+              <div className="card card1 pt-4">
+                  <div className="icon">
+                     <i className="fa-solid fa-envelope icon-color"></i> 
                   </div>
-                {/* <img src="..." class="card-img-top" alt="..."> */}
-                <div class="card-body">
-                  <h5 class="card-title">E-mail</h5>
-                  <p class="card-text">trackpi@gmail.com</p>
+               
+                <div className="card-body">
+                  <h6 className="card-title">E-mail</h6>
+                  <p className="card-text fw-bold">trackpi@gmail.com</p>
                 </div>
     
           </div>
         </div>
       </div>
       </div>
-      <section className="mt-32 w-full">
-        <Container className="flex flex-col items-center text-justify">
-          <Row className="mt-5 md:w-1/2">
-              <h1 className="mb-3 fw-bold display-5">
+      <section className=" w-full">
+        <Container className="text-center">
+          <Row className="mt-5">
+              <h1 className="mb-3 fw-bold connect-text-Color ">
                 You’re ready to <br /> take the next step
               </h1>
-              <p className="mb-5 text-xl md:text-justify md:text-lg">
+              <p className="mb-5">
                 We’re all wrestling with complexity. Every company, work function,
                 and team now faces a tall order: to be more adaptive, strategic,
                 effective, human, and equitable amidst growing uncertainty.
               </p>
+              </Row>
+              </Container>
+              <Container className='flex flex-col items-center'>
+              <div className=' md:w-1/2 '>
               <Details/>
-          </Row>
-        </Container>
+              </div>
+              </Container>
       </section>
       <ConnectButtons/>
     </div>
