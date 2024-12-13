db735a4cb73c252980ab7861edaddd2c53b23b57
Nidha70 <fathimanidhapk@gmail.com>
2024-12-13 20:13:45 +0530

    updated above

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,17 +8,24 @@ function About() {
   return (
     <>
         <PopUp/>
-      <section className="flex justify-center items-center p-3 w-full h-screen about_section bg1 relative">
-        <div className="flex flex-col items-center z-10">
-          <h1 className="text-5xl md:text-7xl  working_heading mb-5">
-            About Us
-          </h1>
-          <p className="font-semibold text-md md:text-2xl working_subheading text-center">
-            Learn more about our journey and what drives us forward.
-          </p>
-          <Brochure />
-        </div>
-      </section>
+        
+        <Brochure /> 
+          <section className="flex justify-center items-center p-3 w-full h-screen about_section bg1 relative">
+          
+            <div className="flex flex-col items-center z-10  " >
+              <h1 className="text-5xl md:text-7xl  working_heading mb-5">
+                About Us
+              </h1>
+              <p className="font-semibold text-md md:text-2xl working_subheading text-center">
+                Learn more about our journey and what drives us forward.
+              </p>
+              
+            </div>
+            
+          </section>
+          
+        
+      
 
       <section className="mt-12">
         <OurTeam />
