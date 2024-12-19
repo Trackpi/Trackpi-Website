2f5f28c11633a21687ba596edf42fb920390899f
Nidha70 <fathimanidhapk@gmail.com>
2024-12-19 11:02:46 +0530

    pushing again

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter
       future={{
         v7_relativeSplatPath: true,
+        v7_startTransition: true
       }}
     >
       <App />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 // Created by Shalu
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import { motion } from 'framer-motion';
+import { useInView } from '../components/UseInView';
 import { Link } from 'react-router-dom';
 import home1 from '../images/home1.png';
 import home2 from '../images/home2.png';
 import ConnectButtons from '../components/ConnectButtons';
-import { FaAngleRight } from 'react-icons/fa6';
-import { FaAngleLeft } from 'react-icons/fa6';
+
 import logo from '../images/trackpi_logo.png';
 import { Carousel } from 'react-bootstrap';
 import clientLogo1 from '../images/growthfactor.png';
@@ -14,6 +15,10 @@ import clientLogo2 from '../images/trademax.png';
 import HeaderBanner from '../components/HeaderBanner';
 
 function Home() {
+  const isInView1 = useInView({ selector: '.section1' });
+  const isInView2 = useInView({ selector: '.section2' });
+  const isInView3 = useInView({ selector: '.section3' });
+
   const cards = [
     {
       id: 1,
@@ -132,21 +137,21 @@ function Home() {
   const [groupedCards, setGroupedCards] = useState([]);
   const cardsPerGroup = window.innerWidth < 768 ? 1 : 4; // Responsive: 1 card for mobile, 4 for desktop
   useEffect(() => {
-    // Group cards into sets of 4 (or 1 for mobile)
     const groups = [];
     for (let i = 0; i < cards.length; i += cardsPerGroup) {
       groups.push(cards.slice(i, i + cardsPerGroup));
     }
     setGroupedCards(groups);
-  }, [cards]);
+  }, []);
 
   useEffect(() => {
-    // Auto-bulge effect within a slide
     const bulgeInterval = setInterval(() => {
       setBulgingCard(prev => {
         if (prev === cardsPerGroup - 1) {
           // Move to the next slide after the last card bulges
-          handleNextSlide();
+          setCurrentIndex(prevIndex =>
+            prevIndex === groupedCards.length - 1 ? 0 : prevIndex + 1
+          );
           return 0;
         }
         return prev + 1;
@@ -154,7 +159,7 @@ function Home() {
     }, 2000); // Bulge interval (2 seconds)
 
     return () => clearInterval(bulgeInterval);
-  }, [currentIndex]);
+  }, [groupedCards.length, cardsPerGroup]);
 
   const handleNextSlide = () => {
     setCurrentIndex(prev => (prev === groupedCards.length - 1 ? 0 : prev + 1));
@@ -177,15 +182,12 @@ function Home() {
         titleTwo="Your Strategic Growth Partner"
       />
       <section>
-        <div className="text-center pt-12 pb-6">
-          <h1
-            className="text-6xl  font-bold text-[#FFC100]"
-            style={{ textShadow: '#c3c5c7 2px 4px' }}
-          >
+        <div className="text-center py-12  animate-fade-in">
+          <h1 className="text-5xl  font-bold text-[#FFC100]">
             Real-Time Business News Updates?
           </h1>
         </div>
-        <div className="relative bg-yellow-500">
+        <div className="relative bg-gradient-to-r from-[#FFC100]  to-[#FF9D00]">
           {/* Carousel */}
           <div className="overflow-x-auto md:overflow-hidden touch-pan-x">
             <Carousel
@@ -207,9 +209,9 @@ function Home() {
                     {group.map((card, cardIndex) => (
                       <div
                         key={card.id}
-                        className={`flex-shrink-0 w-full bg-black p-6 rounded-lg shadow-lg text-center transform transition-transform duration-500 cursor-pointer ${
+                        className={`flex-shrink-0 w-full 2xl:h-[490px] bg-black p-6 rounded-lg shadow-lg text-center transform transition-transform duration-500 cursor-pointer ${
                           cardIndex === bulgingCard
-                            ? 'md:scale-110 md:shadow-2xl'
+                            ? 'md:scale-110'
                             : 'scale-100'
                         }`}
                       >
@@ -217,8 +219,8 @@ function Home() {
                         <div
                           className={`transform transition-transform duration-500 ${
                             cardIndex === bulgingCard
-                              ? 'md:scale-105'
-                              : 'scale-100'
+                              ? 'md:scale-105 '
+                              : 'scale-100 '
                           }`}
                         >
                           <img
@@ -233,7 +235,7 @@ function Home() {
                           <h3
                             className={`mt-4 font-bold text-amber-500 transition-transform ${
                               cardIndex === bulgingCard
-                                ? 'md:text-xl'
+                                ? 'md:text-xl '
                                 : 'text-lg'
                             }`}
                           >
@@ -278,8 +280,8 @@ function Home() {
         </div>
       </section>
 
-      <section className="w-full h-full">
-        <Row className="mt-5 px-10">
+      <section className="w-full h-full mt-20">
+        <Row className="mt-5 text-center">
           <h1 className="text-[#FFC100] font-extrabold">OUR CLIENTS</h1>
           <h4 className="fw-bold text-[#0A0A0A]">
             We’re fortunate to work with the best
@@ -304,53 +306,86 @@ function Home() {
           </div>
         </Row>
       </section>
-      <section className="my-16 w-full h-full px-10">
-        <div className="flex flex-col md:flex-row gap-4 items-center">
-          {/* Text Container */}
+
+      <section className="mt-28 w-full px-6 lg:px-20 xl:px-24 2xl:px-32 mx-auto section1">
+        <motion.div
+          className="flex flex-col md:flex-row gap-12 lg:gap-20 xl:gap-20 2xl:gap-32 items-center"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: isInView1 ? 1 : 0, y: isInView1 ? 0 : 50 }}
+          transition={{ duration: 1 }}
+        >
+          {/* Text and Image Content */}
           <div className="md:w-1/2">
-            <h1 className="fw-bold text-[#FFC100] text-5xl pb-2">
+            <motion.h1
+              className="font-bold text-[#FFC100] text-3xl xl:text-5xl 2xl:text-6xl pb-2"
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: isInView1 ? 1 : 0, x: isInView1 ? 0 : -50 }}
+              transition={{ duration: 1 }}
+            >
               We see the challenge
-            </h1>
-            <p className="text-justify  text-sm md:text-lg text-[#0A0A0A]">
+            </motion.h1>
+            <motion.p
+              className="text-justify text-sm sm:text-lg md:text-2xl xl:text-xl xl:leading-7 2xl:leading-10 2xl:text-3xl text-[#0A0A0A]"
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: isInView1 ? 1 : 0, x: isInView1 ? 0 : -50 }}
+              transition={{ duration: 1, delay: 0.2 }}
+            >
               We’re all wrestling with complexity. Every company, work function,
               and team now faces a tall order: to be more adaptive, strategic,
               effective, human, and equitable amidst growing uncertainty.
-            </p>
+            </motion.p>
           </div>
-
-          {/* Image Container */}
           <div className="md:w-1/2 flex justify-end">
-            <img
+            <motion.img
               src={home1}
               alt="Strategic Procurement"
-              className="shadow-lg rounded-lg w-full max-w-[590px] h-[300px] md:h-[490px] object-cover"
+              className="shadow-lg rounded-lg w-full h-[300px] md:h-[390px] 2xl:h-[690px] object-cover"
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{
+                opacity: isInView1 ? 1 : 0,
+                scale: isInView1 ? 1 : 0.9,
+              }}
+              transition={{ duration: 1 }}
             />
           </div>
-        </div>
+        </motion.div>
       </section>
 
-      <section
-        className=" w-full h-full flex flex-col  md:flex-row items-center px-10  py-12 relative bg2 text-black"
-        style={{ backgroundColor: '#FFC100' }}
-      >
-        <div className="flex flex-col md:flex-row gap-4 items-center">
-          {/* Image Container */}
+      <section className="mt-28 w-full px-6 lg:px-20 xl:px-24 2xl:px-32 mx-auto py-20 bg-[#FFC100] text-black bg2 section2">
+        <motion.div
+          className="flex flex-col md:flex-row gap-12 lg:gap-20 xl:gap-20 2xl:gap-32 items-center"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: isInView2 ? 1 : 0, x: isInView2 ? 0 : 50 }}
+          transition={{ duration: 1 }}
+        >
+          {/* Image and Text Content */}
           <div className="md:w-1/2 flex justify-start">
-            <img
+            <motion.img
               src={home1}
               alt="Strategic Procurement"
-              className="shadow-lg rounded-lg w-full max-w-[590px] h-[300px] md:h-[490px] object-cover"
+              className="shadow-lg rounded-lg w-full h-[300px] md:h-[390px] 2xl:h-[690px] object-cover"
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{
+                opacity: isInView2 ? 1 : 0,
+                scale: isInView2 ? 1 : 0.9,
+              }}
+              transition={{ duration: 1 }}
             />
           </div>
-
-          {/* Text Container */}
-          <div className="md:w-1/2 ">
-            <h1 className="md:pt-5 fw-bold text-5xl">
-              We need to shift <br /> our thinking
-            </h1>
-            <p
-              className="text-justify text-sm md:text-lg"
-              style={{ marginTop: '1.5rem' }}
+          <div className="md:w-1/2">
+            <motion.h1
+              className="font-bold text-3xl xl:leading-tight xl:text-5xl 2xl:text-6xl pb-2"
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: isInView2 ? 1 : 0, x: isInView2 ? 0 : 50 }}
+              transition={{ duration: 1 }}
+            >
+              We need to shift our thinking
+            </motion.h1>
+            <motion.p
+              className="text-justify text-sm sm:text-lg md:text-2xl xl:text-xl xl:leading-7 2xl:leading-10 2xl:text-3xl text-[#0A0A0A]"
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: isInView2 ? 1 : 0, x: isInView2 ? 0 : 50 }}
+              transition={{ duration: 1, delay: 0.2 }}
             >
               Organizations are human systems full of potential to navigate
               complexity, design human processes, and make meaningful change.
@@ -358,85 +393,133 @@ function Home() {
               accepts that organizations aren't machines. It requires having the
               courage to say no to the status quo and yes to building
               future-ready capabilities.
-            </p>
+            </motion.p>
           </div>
-        </div>
+        </motion.div>
       </section>
+
 
       <section className="relative z-20"></section>
 
-      <section className="px-10 py-10 w-full h-full">
-        <h1 className=" font-bold text-[#FFC100] text-5xl">
-          The old ways of working aren’t the <br /> only ways of working
-        </h1>
+      <section className="px-6 lg:px-20 xl:px-24 2xl:px-32 mx-auto w-full h-full mt-28 section3">
+      <motion.h1
+        className="font-bold text-[#FFC100] text-5xl 2xl:text-7xl"
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: isInView3 ? 1 : 0, y: isInView3 ? 0 : -50 }}
+        transition={{ duration: 1 }}
+      >
+        The old ways of working aren’t the <br /> only ways of working
+      </motion.h1>
 
-        <div className="flex flex-col md:flex-row items-center gap-4">
-          {/* Text Content */}
-          <div className="flex flex-col  w-full md:w-1/2">
-            <p className="font-bold text-lg md:text-2xl mb-4">
-              We help organizations evolve new practices:
-            </p>
-
-            <p className="text-base md:text-xl mb-3">
-              From struggling to attract and retain top talent To giving
-              everyone a voice in shaping the organization
-            </p>
-
-            <p className="text-base md:text-xl mb-3">
-              From hoarding information To making nearly all information
-              transparent and accessible
-            </p>
-
-            <p className="text-base md:text-xl mb-3">
-              From being bombarded with meetings and email To shedding status
-              meetings and bureaucratic theater
-            </p>
-
-            <p className="text-base md:text-xl mb-3">
-              From hitting bottlenecks in decision-making To enabling
-              safe-to-try decisions at the edge
-            </p>
-
-            <p className="text-base md:text-xl mb-3">
-              From obsessing over short-term results To choosing strategic
-              priorities and explicit tradeoffs
-            </p>
-          </div>
-
-          {/* Image Content */}
-          <div className="w-full md:w-1/2 flex justify-center md:justify-end">
-            <img
-              src={home2}
-              alt="Strategic Procurement"
-              className="shadow-lg rounded-lg w-full max-w-[590px] h-[300px] md:h-[490px] object-cover"
-            />
-          </div>
-        </div>
-      </section>
-
-      <section className="flex justify-center px-10 py-12 h-full w-full relative mb-14 bg-yellow-500 shadow-[16px_14px_16px_rgba(0,0,0,0.2)]">
-        <div className="flex flex-col gap-4 justify-center items-center text-center">
-          <h1 className="text-black font-bold text-2xl md:text-3xl">
-            We're Ready to Help
-          </h1>
-
-          <p className="text-md md:text-xl text-black px-4 md:px-12 text-justify">
-            How we work is broken, dehumanizing, and held back by bureaucracy.
-            But it can be reinvented in service of human flourishing even joy.
-            These changes aren't at the expense of business outcomes. Instead,
-            they're fuel for even greater ambitions. The Ready helps
-            organizations accelerate that change as fast, far, and wide as
-            possible.
-          </p>
-
-          <a
-            href="/our-services"
-            className="bg-black text-white font-semibold mt-3 px-6 py-2 rounded-full shadow-lg hover:bg-gray-800 transition duration-300"
+      <motion.div
+        className="flex flex-col md:flex-row gap-12 lg:gap-20 xl:gap-20 2xl:gap-32 items-center"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: isInView3 ? 1 : 0, y: isInView3 ? 0 : 50 }}
+        transition={{ duration: 1 }}
+      >
+        {/* Text Content */}
+        <div className="flex flex-col w-full md:w-1/2">
+          <motion.p
+            className="font-bold text-lg md:text-2xl 2xl:text-4xl mb-4"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: isInView3 ? 1 : 0, x: isInView3 ? 0 : -50 }}
+            transition={{ duration: 1, delay: 0.2 }}
           >
-            OUR SERVICES
-          </a>
+            We help organizations evolve new practices:
+          </motion.p>
+
+          <motion.p
+            className="text-base md:text-xl 2xl:text-3xl mb-3"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: isInView3 ? 1 : 0, x: isInView3 ? 0 : -50 }}
+            transition={{ duration: 1, delay: 0.4 }}
+          >
+            From struggling to attract and retain top talent To giving
+            everyone a voice in shaping the organization
+          </motion.p>
+
+          <motion.p
+            className="text-base md:text-xl 2xl:text-3xl mb-3"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: isInView3 ? 1 : 0, x: isInView3 ? 0 : -50 }}
+            transition={{ duration: 1, delay: 0.6 }}
+          >
+            From hoarding information To making nearly all information
+            transparent and accessible
+          </motion.p>
+
+          <motion.p
+            className="text-base md:text-xl 2xl:text-3xl mb-3"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: isInView3 ? 1 : 0, x: isInView3 ? 0 : -50 }}
+            transition={{ duration: 1, delay: 0.8 }}
+          >
+            From being bombarded with meetings and email To shedding status
+            meetings and bureaucratic theater
+          </motion.p>
+
+          <motion.p
+            className="text-base md:text-xl 2xl:text-3xl mb-3"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: isInView3 ? 1 : 0, x: isInView3 ? 0 : -50 }}
+            transition={{ duration: 1, delay: 1 }}
+          >
+            From hitting bottlenecks in decision-making To enabling
+            safe-to-try decisions at the edge
+          </motion.p>
+
+          <motion.p
+            className="text-base md:text-xl 2xl:text-3xl mb-3"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: isInView3 ? 1 : 0, x: isInView3 ? 0 : -50 }}
+            transition={{ duration: 1, delay: 1.2 }}
+          >
+            From obsessing over short-term results To choosing strategic
+            priorities and explicit tradeoffs
+          </motion.p>
         </div>
-      </section>
+
+        {/* Image Content */}
+        <div className="w-full md:w-1/2 flex justify-center md:justify-end">
+          <motion.img
+            src={home2}
+            alt="Strategic Procurement"
+            className="shadow-lg rounded-lg w-full h-[300px] md:h-[390px] 2xl:h-[690px] object-cover"
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: isInView3 ? 1 : 0, scale: isInView3 ? 1 : 0.9 }}
+            transition={{ duration: 1 }}
+          />
+        </div>
+      </motion.div>
+    </section>
+
+      <div className="shadow-bottom">
+        <section className="flex justify-center px-6 lg:px-20 xl:px-24 2xl:px-32 mx-auto py-16 h-full w-full relative mt-20 mb-12 bg-[#FFC100] bg3">
+          <div className="flex flex-col gap-2 justify-center items-center text-center">
+            <h1 className="text-black font-bold text-3xl xl:leading-tight xl:text-5xl 2xl:text-6xl pb-2">
+              We're Ready to Help
+            </h1>
+            <p className="text-center text-sm sm:text-lg md:text-2xl xl:text-xl xl:leading-7 2xl:leading-10 2xl:text-3xl text-black">
+              How we work is broken, dehumanizing, and held back by bureaucracy.
+              But it can be reinvented in service of human flourishing even joy.
+              These changes aren't at the expense of business outcomes. Instead,
+              they're fuel for even greater ambitions. The Ready helps
+              organizations accelerate that change as fast, far, and wide as
+              possible.
+            </p>
+            <Link to="/our-services">
+              <button
+                className="bg-black no-underline text-white font-semibold mt-3 px-4 py-2 text-sm 
+        md:px-6 md:py-3 md:text-base lg:px-8 lg:py-4 lg:text-lg xl:px-10 xl:py-5 xl:text-xl 
+        2xl:px-12 2xl:py-6 2xl:text-2xl rounded-full shadow-lg hover:bg-gray-800 
+        transition duration-300"
+              >
+                OUR SERVICES
+              </button>
+            </Link>
+          </div>
+        </section>
+      </div>
 
       <ConnectButtons />
     </>
