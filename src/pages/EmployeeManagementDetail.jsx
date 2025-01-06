bbada6c934549ade906ad89b161a5db0b2e59d83
Nidha70 <fathimanidhapk@gmail.com>
2025-01-06 15:36:26 +0530

    employee

--- src/pages/EmployeeManagementDetail.jsx
+++ src/pages/EmployeeManagementDetail.jsx
@@ -1,8 +1,13 @@
 
+import React from "react";
 
-import React from 'react'
-import Personel from './personel'
 
+import { IoLogoInstagram } from "react-icons/io5";
+
+import { RiFacebookCircleLine } from "react-icons/ri";
+import { TbBrandLinkedin } from "react-icons/tb";
+
+import { Modal } from "react-bootstrap";
 
 
 
@@ -10,7 +15,55 @@ function EmployeeManagementDetail() {
  
   return (
     <div>
-     <Personel/>
+      <Modal show={show} onHide={onHide} centered size="md">
+            <Modal.Header className="bg-white px-1 py-1 text-center text-black" >
+              <Modal.Title className="personnelfirst" >
+                <h2>{member.title}</h2>
+                <p className="text-2xl font-normal">{member.designation}</p>
+                <button
+                  onClick={onHide}
+                  style={{
+                    position: "absolute",
+                    top: "10px",
+                    right: "10px",
+                    border: "none",
+                    borderRadius: "50%",
+                    color: "black",
+                    width: "30px",
+                    height: "30px",
+                    cursor: "pointer",
+                    fontSize: "30px",
+                  }}
+                >
+                  &times;
+                </button>
+                </Modal.Title>
+                <img
+                  src={member.image}
+                  alt={member.title}
+                  className="rounded-md w-52 h-52 personnelimg object-cover"
+                />
+            </Modal.Header>
+            <Modal.Body className="px-2  py-4 flex gap-50 ">
+              <div>
+               
+                <div className="  px-8 flex flex-col justify-center items-center ">
+                  
+                  <p className="mt-4 font-medium text-justify"><br/><br/><br/>
+                  <span className="font-bold underline">SELF INTRODUCTION:</span>
+                  &nbsp;
+                  {member.selfIntroduction}</p>
+                </div>
+                <div className=" flex justify-evenly gap-20 mt-6 px-10">
+                <RiFacebookCircleLine size={35} className="text-yellow-500 cursor-pointer" />
+               
+                <IoLogoInstagram size={35} className="text-yellow-500 cursor-pointer" />
+                <TbBrandLinkedin size={35} className="text-yellow-500 cursor-pointer" />
+              </div>
+              </div>
+            </Modal.Body>
+            
+          </Modal>
      
     </div>
   )
--- src/pages/EmployeeSales.jsx
+++ src/pages/EmployeeSales.jsx
@@ -1,23 +1,14 @@
 
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import EmpDetails from "../components/EmpDetails";
 import "../CSS/employeeinternship.css";
 function EmployeeSales() {
-  const [headerHeight, setHeaderHeight] = useState(0);
-  useEffect(() => {
-    
-    const header = document.querySelector(".navbar"); 
-  
-    if (header) {
-      setHeaderHeight(header.offsetHeight + 30); 
-    }
-  }, []);
-
+ 
 
   return (
-    <div className="pad   w-full py-5 md:py-10 md:px-6 lg:px-20 xl:px-24 2xl:px-32 mx-auto " style={{ paddingTop: headerHeight }} >
+    <div className="pad mt-20  w-full py-5 md:py-10 md:px-6 lg:px-20 xl:px-24 2xl:px-32 mx-auto "  >
        <EmpDetails/>
        <div className="  text-center flex justify-center mt-19 md:mt-19 2xl:mt-8 ">
                   
